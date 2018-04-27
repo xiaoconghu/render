8e3a0d77d36a544a26522abfffacb1447ed255dc
xiaoconghu <huxc10086@163.com>
2018-04-27 17:34:42 +0800

    jit 模式动态创建组件

---
+++ src/app/custom-template/custom-template.component.ts
@@ -0,0 +1,67 @@
+import {
+  Component, OnInit, Input, NgModule, NgModuleFactory, Compiler
+} from '@angular/core';
+
+interface Bindings {
+  [key: string]: any;
+}
+
+@Component({
+  selector: 'app-custom-template',
+  template: `
+    <ng-container *ngComponentOutlet="dynamicComponent;
+                            ngModuleFactory: dynamicModule;"></ng-container>`,
+  styleUrls: ['./custom-template.component.css']
+})
+export class CustomTemplateComponent implements OnInit {
+  dynamicComponent;
+  dynamicModule: NgModuleFactory<any>;
+
+  @Input()
+  text: string;
+  @Input()
+  public bindings: Bindings = {};
+
+  constructor(private compiler: Compiler) {
+  }
+
+  ngOnInit() {
+    this.dynamicComponent = this.createNewComponent(this.text, this.bindings);
+    this.dynamicModule = this.compiler.compileModuleSync(this.createComponentModule(this.dynamicComponent));
+  }
+
+  protected createComponentModule(componentType: any) {
+    @NgModule({
+      imports: [],
+      declarations: [
+        componentType
+      ],
+      entryComponents: [componentType]
+    })
+    class RuntimeComponentModule {
+    }
+
+    // a module for just this Type
+    return RuntimeComponentModule;
+  }
+
+  protected createNewComponent(text: string, bindings: Bindings) {
+    const template = `dynamically created template with text: ${text}`;
+
+    @Component({
+      selector: 'app-dynamic-component',
+      template: template
+    })
+    class DynamicComponent implements OnInit {
+      text: any;
+      public bindings: Bindings;
+
+      ngOnInit() {
+        this.text = text;
+        this.bindings = bindings;
+      }
+    }
+
+    return DynamicComponent;
+  }
+}
